c4f6d3f63a6d9228cbaa7597251654f0f6a451f5
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-07-16 11:48:36 -0500

    probamos si conecta el cliente

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 //llamamos al paquete expres
-const express = require('express')
+const express = require('express');
 //creamos app de express
-const app = express()
+const app = express();
 //llamamos al paquete http de nodejs y le pasamos express
 var server = require('http').Server(app);
 //llamamos al paquete de socket.io y le pasamos el servidor
 var io = require('socket.io')(server);
+
+//le decimos a express que esa sera la carpeta de file static
+app.use(express.static('client'));
 
 //creamos una ruta por default
 app.get('/', function(req, res){
